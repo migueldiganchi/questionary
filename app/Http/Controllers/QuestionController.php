51bf82e991720286b4235f9b80d66eb0844614c2
Alejandro Barabasz <alejandro.barabasz@elserver.com>
2015-07-31 18:30:33 -0300

    Added questions controllers for index and show

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -8,12 +8,51 @@ use App\UserSession;
 class QuestionController extends Controller
 {
 
-    public function index(Request $request) {
+	public function index(Request $request) {
 
 		$session = UserSession::current();
-    	$user = $session->user;
+		$user = $session->user;
+		$questions = $user->questions()->orderBy('order', 'asc')->get();
+		
+		return view('question.index', compact('session', 'user', 'questions'));
+	}
 
-       	return view('question.index', compact('user'));
-    }
-    
+
+	public function create(Request $request) {
+		$session = UserSession::current();
+		$user = $session->user;
+		
+		return view('question.create', compact('session', 'user'));
+	}
+
+
+	public function store(Request $request) {
+// 		$input = $request->only('text');
+// 		$answers = (array) $request->input('answers', array());
+	}
+
+
+	public function show(Request $request, $id) {
+		$session = UserSession::current();
+		$user = $session->user;
+		$question = $user->questions()->find($id);
+		
+		return view('question.show', compact('session', 'user', 'question'));
+	}
+
+
+	public function edit(Request $request, $id) {
+		
+	}
+
+
+	public function update(Request $request, $id) {
+		
+	}
+
+
+	public function destroy(Request $request, $id) {
+		
+	}
+
 }
--- resources/views/question/index.blade.php
+++ resources/views/question/index.blade.php
@@ -3,8 +3,18 @@
 @section('content')
 
 	<!-- <img src="https://graph.facebook.com/facebook_user_id/picture?type=small"> -->
-
-	<h1>Questions list</h1>
-
+	<h2>Tus Preguntas</h2>
+	
+	@if ($questions)
+		<ul class="list questions-list">
+			@foreach($questions as $question)
+				<li class="question">
+					<p><a href="#" data-question-id="{{$question->id}}">{{ $question->text }}</a></p>
+				</li>
+			@endforeach
+		</ul>
+	@else
+		<div class="alert alert-info" role="alert">A&uacute;n no tienes preguntas!</div>
+	@endif
 
 @endsection
