217343e243ecb9d128bb4d981203474e02ae4f81
Alejandro Barabasz <alejandro.barabasz@elserver.com>
2015-07-31 18:31:24 -0300

    Added matches controllers for index and show

---
+++ app/Http/Controllers/MatchController.php
@@ -0,0 +1,56 @@
+<?php namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Controllers\Controller;
+use App\UserSession;
+
+class MatchController extends Controller
+{
+
+	public function index(Request $request) {
+		$session = UserSession::current();
+		$user = $session->user;
+		$matches = $user->matches()->with(['host', 'guest'])->orderBy('order', 'asc')->get();
+		
+		return view('match.index', compact('session', 'user', 'matches'));
+	}
+
+
+	public function create(Request $request) {
+		$session = UserSession::current();
+		$user = $session->user;
+		
+		return view('match.create', compact('session', 'user'));
+	}
+
+
+	public function store(Request $request) {
+		
+	}
+
+
+	public function show(Request $request, $id) {
+		$session = UserSession::current();
+		$user = $session->user;
+		$match = $user->matches()->with(['host', 'guest'])->find($id);
+		
+		return view('match.show', compact('session', 'user', 'match'));
+	}
+
+
+	public function edit(Request $request, $id) {
+		
+	}
+
+
+	public function update(Request $request, $id) {
+		
+	}
+
+
+	public function destroy(Request $request, $id) {
+		
+	}
+
+}
---
+++ resources/views/match/index.blade.php
@@ -0,0 +1,25 @@
+@extends('app')
+
+@section('content')
+
+	<h2>Tus Partidas</h2>
+	
+	@if ($matches)
+		<ul class="list matches-list">
+			@foreach($matches as $match)
+				<li class="match">
+					<p>
+						<a href="#" data-match-id="{{$match->id}}">
+							@if ($match->host_id == $user->id) {{ $match->guest->name }} @else {{ $match->host->name }} @endif
+							-
+							{{ date('d-m-Y H:i:s', strtotime($match->created_at)) }}
+						</a>
+					</p>
+				</li>
+			@endforeach
+		</ul>
+	@else
+		<div class="alert alert-info" role="alert">A&uacute;n no tienes partidas!</div>
+	@endif
+
+@endsection
