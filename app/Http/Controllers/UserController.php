86d31d1e9d48fcd93e717eb42f4cf3a29cb36c8f
Alejandro Barabasz <alejandro.barabasz@elserver.com>
2015-08-13 17:06:44 -0300

    Fixed validations and responses in User controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,164 +17,153 @@ use Facebook\FacebookJavaScriptLoginHelper;
 
 class UserController extends Controller
 {
-    public function login() {
+	public function login() {
 
-        $fb_scopes = array('public_profile', 'email', 'read_custom_friendlists', 'user_friends');
+		$fb_scopes = array('public_profile', 'email', 'read_custom_friendlists', 'user_friends');
 
-        $fb_helper = new FacebookRedirectLoginHelper(route('user.auth'));
+		$fb_helper = new FacebookRedirectLoginHelper(route('user.auth'));
 
-        return view('user.login', compact('fb_helper', 'fb_scopes'));
-    }
+		return view('user.login', compact('fb_helper', 'fb_scopes'));
+	}
 
-    public function auth(Request $request, Response $response)
-    {
-        $fb_helper = new FacebookRedirectLoginHelper(route('user.auth'));
+	public function auth(Request $request, Response $response)
+	{
+//		$fb_helper = new FacebookRedirectLoginHelper(route('user.auth'));
+//
+//		try {
+//			$fb_session = $fb_helper->getSessionFromRedirect();
+//		} catch(FacebookRequestException $ex) {
+//			$this->showFormatedObject($ex, 'ex'); exit();
+//		} catch(\Exception $ex) {
+//			$this->showFormatedObject($ex, 'ex'); exit();
+//		}
+//
+//		// check if the user has been logged
+//		if (!$fb_session) {
+//			return redirect('user.login')->with('error_message', 'El usuario no tiene cuenta.');
+//		}
+//
+//		// read data from the user
+//		$user = $this->getOrCreateUser($fb_session);
+//
+//		// create new session for the user
+//		$new_session = $this->createSession($user, $fb_session, $request->getClientIp());
+//
+//		// set sessions vars
+//		return redirect()->route('question.index')->withCookies(array(
+//			cookie(UserSession::$COOKIE_NAME, $new_session->session_id, UserSession::$LIFE_TIME),
+//		));
+	}
 
-        try {
-          $fb_session = $fb_helper->getSessionFromRedirect();
-        } catch(FacebookRequestException $ex) {
-          $this->showFormatedObject($ex, 'ex'); exit();
-        } catch(\Exception $ex) {
-          $this->showFormatedObject($ex, 'ex'); exit();
-        }
-        
-        // check if the user has been logged
-        if (!$fb_session) {
-            return redirect('user.login')->with('error_message', 'El usuario no tiene cuenta.');
-        }
-        
-        // read data from the user
-        $user = $this->getOrCreateUser($fb_session);
-        
-        // create new session for the user
-        $new_session = $this->createSession($user, $fb_session, $request->getClientIp());
+	public function authFacebook(Request $request, Response $response) {
 
-        // set sessions vars
-        return redirect()->route('question.index')->withCookies(array(
-            cookie(UserSession::$COOKIE_NAME, $new_session->session_id, UserSession::$LIFE_TIME),
-        ));
-    }
+		try {
+			// Instance Facebook Helper
+			$fb_helper = new FacebookJavaScriptLoginHelper();
+			
+			// Get Facebook Session
+			$fb_session = $fb_helper->getSession();
+			
+			// check if the user has been logged
+			if (!$fb_session) {
+				return $this->responseJsonError('no_user_account', 'El usuario no se encuentra logueado.');
+			}
+			
+			// Get or create the user
+			$user = $this->getOrCreateUser($fb_session);
 
-    public function authFacebook(Request $request, Response $response) {
+			// Create new session for the user
+			$new_session = $this->createSession($user, $fb_session, $request->getClientIp());
 
-        $fb_helper = new FacebookJavaScriptLoginHelper();
+			// Set session cookie
+			UserSession::setCookie($new_session->session_id);
+			
+			// return json success data
+			return $this->responseJsonSuccess([
+				'session_id' => $new_session->session_id,
+			]);
 
-        try {
+		} catch(FacebookRequestException $ex) {
+			// @todo: log real message: $ex->getMessage()
+			return response()->json([
+				'success' => 0, 
+				'error' => array (
+					'code' => 'facebook_error',
+					'message' => 'Se ha producido un error al leer la sesión del usuario.'
+				)
+			]);
 
-            $fb_session = $fb_helper->getSession();
+		} catch(\Exception $ex) {
+			// @todo: log real message: $ex->getMessage()
+			return $this->responseJsonError('app_error', 'Se ha producido un error al leer la sesión del usuario.');
+		}
+	}
 
-        } catch(FacebookRequestException $ex) {
-            // @todo: log real message: $ex->getMessage()
-            return response()->json([ 'success' => 0, 
-                'error' => array (
-                    'code' => 'facebook_error',
-                    'message' => 'Se ha producido un error al leer la sesión del usuario.'
-                )
-            ]);
+	public function logout() {
+		// Unset current session
+		UserSession::unsetCurrent();
+		
+		// Remove session cookie
+		UserSession::removeCookie();
+		
+		// redirect to Home page
+		return redirect()->route('home.index');
+	}
 
-        } catch(\Exception $ex) {
-            // @todo: log real message: $ex->getMessage()
-            return response()->json(['success' => 0, 
-                'error' => array (
-                    'code' => 'app_error',
-                    'message' => 'Se ha producido un error al leer la sesión del usuario.'
-                )
-            ]);
+	private function getOrCreateUser($fb_session) {
 
-        }
+		// Get facebook user
+		$me = (new FacebookRequest($fb_session, 'GET', '/me'))->execute()->getGraphObject(GraphUser::className());
+		
+		// Get the facebook profile ID
+		$fb_id = $me->getProperty('id');
 
-        // check if the user has been logged
-        if (!$fb_session) {
-            return response()->json(['success' => 0, 
-                'error'     => array (
-                    'code'      => 'no_user_account',
-                    'message'   => 'El usuario no se encuentra logueado.'
-                )
-            ]);
-        }
+		// Check if user exists for current facebook ID.
+		$user = User::where('fb_id', '=', $fb_id)->first();
 
-        // Get or create the user
-        $user = $this->getOrCreateUser($fb_session);
-        
-        // Create new session for the user
-        $new_session = $this->createSession($user, $fb_session, $request->getClientIp());
+		// If user does not exists, check by email
+		if (!$user) {
+			$user = User::where('email', '=', $me->getProperty('email'))->first();
+		}
 
-        // Set session cookie
-        UserSession::setCookie($new_session->session_id);
-        
-        // return json success data
-        return response()->json(['success' => 1, 
-            'data' => array(
-                'session_id' => $new_session->session_id,
-            )
-        ]);
-    }
+		// If user is not found, create it using facebook profile information
+		if (!$user) {
+			$user = User::create( array(
+				'email' => $me->getProperty('email'),
+				'name' => $me->getProperty('name'),
+				'fb_id' => $fb_id,
+			));
+		}
 
-    public function logout() {
-    	// Unset current session
-    	UserSession::unsetCurrent();
-    	
-    	// Remove session cookie
-    	UserSession::removeCookie();
-    	
-    	// redirect to Home page
-    	return redirect()->route('home.index');
-    }
+		// Set current facebook ID if has a different one
+		if ($user->fb_id != $fb_id) {
+			$user->fb_id = $fb_id;
+		}
 
-    private function getOrCreateUser($fb_session) {
+		// Update user login time
+		$user->last_login = date('Y-m-d H:i:s');
+		$user->save();
 
-        // Get facebook user
-        $me = (new FacebookRequest($fb_session, 'GET', '/me'))->execute()->getGraphObject(GraphUser::className());
-        
-        // Get the facebook profile ID
-        $fb_id = $me->getProperty('id');
+		return $user;
+	}
 
-        // Check if user exists for current facebook ID.
-        $user = User::where('fb_id', '=', $fb_id)->first();
+	private function createSession($user, $fb_session, $ip) {
 
-        // If user does not exists, check by email
-        if (!$user) {
-            $user = User::where('email', '=', $me->getProperty('email'))->first();
-        }
+		// store session
+		$new_session = UserSession::create(array(
+			'session_id' => md5( $user->email . $ip . time() ), 
+			'user_id' => $user->id,
+			'fb_token' => $fb_session->getToken(),
+			'ip' => $ip,
+			'expires_at' => date('Y-m-d H:i:s', time() + UserSession::$LIFE_TIME * 60),
+		));
+		
+		// set current session for the user
+		Cookie::unqueue(UserSession::$COOKIE_NAME);
+		UserSession::current($new_session);
 
-        // If user is not found, create it using facebook profile information
-        if (!$user) {
-            $user = User::create( array(
-                'email' => $me->getProperty('email'),
-                'name' => $me->getProperty('name'),
-                'fb_id' => $fb_id,
-            ));
-        }
-
-        // Set current facebook ID if has a different one
-        if ($user->fb_id != $fb_id) {
-            $user->fb_id = $fb_id;
-        }
-
-        // Update user login time
-        $user->last_login = date('Y-m-d H:i:s');
-        $user->save();
-
-        return $user;
-    }
-
-    private function createSession($user, $fb_session, $ip) {
-
-        // store session
-        $new_session = UserSession::create(array(
-            'session_id' => md5( $user->email . $ip . time() ), 
-            'user_id' => $user->id,
-            'fb_token' => $fb_session->getToken(),
-            'ip' => $ip,
-            'expires_at' => date('Y-m-d H:i:s', time() + UserSession::$LIFE_TIME * 60),
-        ));
-        
-        // set current session for the user
-        Cookie::unqueue(UserSession::$COOKIE_NAME);
-        UserSession::current($new_session);
-
-        return $new_session;
-    }
+		return $new_session;
+	}
 
 }
 
