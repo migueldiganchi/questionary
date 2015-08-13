40c9ad9989ca66a647c7180f3a5b8a293cf092d8
Alejandro Barabasz <alejandro.barabasz@elserver.com>
2015-08-13 17:02:43 -0300

    Fixed layout on views for questions, matches and match invitations

--- resources/views/match/index.blade.php
+++ resources/views/match/index.blade.php
@@ -1,25 +1,19 @@
-@extends('app')
+<h2>Tus Partidas</h2>
 
-@section('content')
-
-	<h2>Tus Partidas</h2>
-	
-	@if ($matches)
-		<ul class="list matches-list">
-			@foreach($matches as $match)
-				<li class="match">
-					<p>
-						<a href="#" data-match-id="{{$match->id}}">
-							@if ($match->host_id == $user->id) {{ $match->guest->name }} @else {{ $match->host->name }} @endif
-							-
-							{{ date('d-m-Y H:i:s', strtotime($match->created_at)) }}
-						</a>
-					</p>
-				</li>
-			@endforeach
-		</ul>
-	@else
-		<div class="alert alert-info" role="alert">A&uacute;n no tienes partidas!</div>
-	@endif
-
-@endsection
+@if ($matches)
+	<ul class="list matches-list">
+		@foreach($matches as $match)
+			<li class="match">
+				<p>
+					<a href="#" data-match-id="{{$match->id}}">
+						@if ($match->host_id == $user->id) {{ $match->guest->name }} @else {{ $match->host->name }} @endif
+						-
+						{{ date('d-m-Y H:i:s', strtotime($match->created_at)) }}
+					</a>
+				</p>
+			</li>
+		@endforeach
+	</ul>
+@else
+	<div class="alert alert-info" role="alert">A&uacute;n no tienes partidas!</div>
+@endif
--- resources/views/question/index.blade.php
+++ resources/views/question/index.blade.php
@@ -1,20 +1,14 @@
-@extends('app')
+<!-- <img src="https://graph.facebook.com/facebook_user_id/picture?type=small"> -->
+<h2>Tus Preguntas</h2>
 
-@section('content')
-
-	<!-- <img src="https://graph.facebook.com/facebook_user_id/picture?type=small"> -->
-	<h2>Tus Preguntas</h2>
-	
-	@if ($questions)
-		<ul class="list questions-list">
-			@foreach($questions as $question)
-				<li class="question">
-					<p><a href="#" data-question-id="{{$question->id}}">{{ $question->text }}</a></p>
-				</li>
-			@endforeach
-		</ul>
-	@else
-		<div class="alert alert-info" role="alert">A&uacute;n no tienes preguntas!</div>
-	@endif
-
-@endsection
+@if ($questions)
+	<ul class="list questions-list">
+		@foreach($questions as $question)
+			<li class="question">
+				<p><a href="#" data-question-id="{{$question->id}}">{{ $question->text }}</a></p>
+			</li>
+		@endforeach
+	</ul>
+@else
+	<div class="alert alert-info" role="alert">A&uacute;n no tienes preguntas!</div>
+@endif
