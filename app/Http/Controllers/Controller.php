23856309c3df42bd87fdfd9a4f97701dbd91a51d
Alejandro Barabasz <alejandro.barabasz@elserver.com>
2015-08-13 16:58:43 -0300

    Added JSON response methods in Base Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,10 +4,49 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
 
 	function showFormatedObject($obj, $obj_name) {
-
 		echo "<pre> {$obj_name} = ". print_r($obj, true) . "</pre>";
 	}
+
+
+	/**
+	 * Creates a successfully JSON Response
+	 * 
+	 * @param mixed $data
+	 * @return \Illuminate\Http\JsonResponse
+	 */
+	function responseJsonSuccess($data) {
+		return response()->json([
+			'success' => 1, 
+			'data' => $data,
+		]);
+	}
+
+	/**
+	 * Creates a JSON Response with errors
+	 * 
+	 * @param string $code
+	 * @param string $message
+	 * @param mixed $data
+	 * @return \Illuminate\Http\JsonResponse
+	 */
+	function responseJsonError($code, $message, $data = null) {
+		// Define error
+		$error = [
+			'code' => $code,
+			'message' => $message,
+		];
+
+		// Attach error data if exists
+		if (isset($data)) {
+			$error['data'] = $data;
+		}
+
+		return response()->json([
+			'success' => 0, 
+			'error' => $error,
+		]);
+	}
+
 }
